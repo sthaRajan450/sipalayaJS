2b96cb6538196e39258af9795f090469a0762d09
rajan shrestha <stharajan477@gmail.com>
2025-04-04 17:34:15 +0545

    git commands

--- CLASS/github.js
+++ CLASS/github.js
@@ -1,1 +1,14 @@
 console.log("github tutorial");
+//add changes
+console.log('git add .')
+
+
+//commit 
+console.log(`git commit -m 'message'`)
+
+//push
+console.log('git push ')
+
+
+//push to branch if any
+console.log('git push origin main')
